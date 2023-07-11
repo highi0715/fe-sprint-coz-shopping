d68a6fa421adfadeceed6bd63e52d257225ed0ce
highi0715 <highi0715@gmail.com>
2023-07-11 18:02:30 +0900

    footer 컴포넌트 구현

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,9 +1,11 @@
 import Header from "../component/Header"
+import Footer from "../component/Footer"
 
 export default function Main() {
     return (
         <section>
             <Header></Header>
+            <Footer></Footer>
         </section>
     )
 }
