d7b25cb4ee025d3352c574e52432e8cfb1551afc
highi0715 <highi0715@gmail.com>
2023-07-14 17:48:40 +0900

    메인페이지 상품리스트 안정화 및 라우트연결

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,15 +1,16 @@
 import './header.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <header className='header-container'>
             <div className='logo-container'>
-                <div className='logo-img'>
-                    <img src="logo.png" alt="logo" />
-                </div>
+                <Link to="/" className='logo-img'>
+                    <img src="/images/logo.png" alt="logo" />
+                </Link>
                 <span className="logo-title">COZ Shopping</span> 
             </div>
-            <img src="hambuger.png" alt="hamburger menu" />
+            <img src="/images/hambuger.png" alt="hamburger menu" />
         </header>
     )
 }
--- src/page/Main.js
+++ src/page/Main.js
@@ -1,17 +1,18 @@
 import Product from "../component/Product/Product"
 import "./main.css"
-
+import { Link } from "react-router-dom";
 export default function Main({products}) {
+    const selectedData = products.slice(0,4);
     return (
         <section>
-            <div className="list-title">상품 리스트</div>
+            <Link to = "/products/list" className="list-title">상품 리스트</Link>
             <ul>
-            {products.map((products) => {
+            {selectedData.map((products) => {
                 return <Product key={products.id} title={products.title} image_url={products.image_url} type={products.type}
                 products={products}></Product>
             })}
             </ul>
-            <div className="list-title">북마크 리스트</div>
+            <Link to="/bookmark"className="list-title">북마크 리스트</Link>
         </section>
     )
 }
